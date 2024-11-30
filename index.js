31618e48c7c72a7e294d7b48ce32646e0a8c4878
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-11-29 21:28:53 -0300

    implemented connection pool

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ const express = require('express')
 const mysql = require('mysql2')
 const app = express()
 app.use(express.json())
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DB, DB_PORT } = process.env
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    database: DB_DB,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+
+
 const porta = 3000
 app.listen(porta, () => console.log(`Executando. Porta ${porta}`))
 
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_DB, DB_PORT} = process.env
 
-function obterConexao() {
-    return mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USER,
-        database: DB_DB,
-        password: DB_PASSWORD,
-        port: DB_PORT
-    })
-}
+
+
 app.get('/medicos', (req, res) => {
 
-    obterConexao().query('SELECT * FROM tb_medico', (err, results, fields) => {
+    pool.query('SELECT * FROM tb_medico', (err, results, fields) => {
         //results tem as linhas
         //fields tem meta dados sobre os resultados, caso estejam disponível
         console.log(results)
@@ -31,23 +38,23 @@ app.get('/medicos', (req, res) => {
 
 app.post('/medicos', (req, res) => {
 
-        const crm = req.body.crm
-        const nome = req.body.nome
+    const crm = req.body.crm
+    const nome = req.body.nome
 
-        const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?,?)"
-        obterConexao().query(sql,[crm,nome], (err, results, fields) => {
-            //results tem as linhas
-            //fields tem meta dados sobre os resultados, caso estejam disponível
-            console.log(results)
-            console.log(fields)
-            console.log(err)
-            res.status(201).send('criado')
-        })
+    const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?,?)"
+    pool.query(sql, [crm, nome], (err, results, fields) => {
+        //results tem as linhas
+        //fields tem meta dados sobre os resultados, caso estejam disponível
+        console.log(results)
+        console.log(fields)
+        console.log(err)
+        res.status(201).send('criado')
+    })
 
 })
 
 app.get('/pacientes', (req, res) => {
-    obterConexao().query('SELECT * FROM tb_paciente', (err, results, fields) => {
+    pool.query('SELECT * FROM tb_paciente', (err, results, fields) => {
         //results tem as linhas
         //fields tem meta dados sobre os resultados, caso estejam disponível
         console.log(results)
